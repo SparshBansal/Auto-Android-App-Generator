d43abcce41f34f9924bf0e7dbacfcacaf47b09f8
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2016-12-28 12:58:35 +0530

    Add NavDrawer and Sliding Tab functionality

--- routes/createApp.js
+++ routes/createApp.js
@@ -17,14 +17,23 @@ router.post('/' , function (req, res, next) {
     var applicationName = req.body.app_name;
     var displayString = req.body.display_string;
 
+    var navigation_type = req.body.navigation_type;
+
+    console.log(navigation_type);
+
     // Now lets create a project directory for the new app
     var packageName = applicationName.replace(/\s/g,"")
     
+    var appDir = "~/generatedProjects/'"+applicationName+"'";
+    var appDirFile = "/home/sparsh/generatedProjects/"+applicationName;
+    
     var mkDirCommand = 'mkdir ~/generatedProjects/'+"'"+applicationName+"'";
     var copyCommand = 'cp -R ~/Projects/AndroidStudioProjects/BaseApplication/* ~/generatedProjects/'+"'"+applicationName+"'";
-    var deleteCommand =  'rm ~/generatedProjects/'+applicationName+"/app/src/main/AndroidManifest.xml";
+  
     var packageUpdateCommand = "find ~/generatedProjects/'"+applicationName+"'/ -type f -exec sed -i 's/com.developer.sparsh.baseapplication/com.amethyst.labs." + packageName + "/g' {} +"
     var buildCommand = "gradle -p ~/generatedProjects/'"+applicationName+"'/" + " assembleDebug";
+
+    var deleteLayoutFileCommand =  'rm ~/generatedProjects/'+applicationName+"/app/src/main/res/layout/activity_main.xml";
 
     var mPromise = new Promise(function (resolve, reject) {
       
@@ -56,12 +65,62 @@ router.post('/' , function (req, res, next) {
         });
       });
 
+    }).then(function(data){
+
+      // Add the asset file to the application
+      var textObj = {};
+      
+      textObj.text = displayString;
+
+      if(navigation_type === 'sliding_tab')
+        textObj.nav_type = 'sliding_tab'
+      else if (navigation_type === 'navigation_drawer')
+        textObj.nav_type = 'navigation_drawer'
+
+      var stringToWrite = JSON.stringify(textObj);
+
+      return new Promise(function(resolve,reject){
+        fs.writeFile(appDirFile+"/app/src/main/assets/data.json" , stringToWrite , function(error){
+          if(error){
+            console.log("error",error);
+            reject(error);
+          }
+          resolve();
+        });
+      });
+
+    }).then(function(log){
+
+      // Delete the activity_main.xml
+      return new Promise(function(resolve,reject){
+        cmd.get(deleteLayoutFileCommand,function(log){
+          resolve(log);
+        });
+      });
+
+    }).then(function(log){
+      
+      // Copy and rename the appropriate xml to resource directory
+      return new Promise(function(resolve,reject){
+        var copyRenameCommand = "";
+        if(navigation_type === 'sliding_tab'){
+          copyRenameCommand = "cp ~/XML/activity_main_slidingtabs.xml ~/generatedProjects/'"+applicationName+"'/app/src/main/res/layout/activity_main.xml"
+        }
+        else if(navigation_type === 'navigation_drawer'){
+          copyRenameCommand = "cp ~/XML/activity_main_navbar.xml ~/generatedProjects/'"+applicationName+"'/app/src/main/res/layout/activity_main.xml"
+        }
+        cmd.get(copyRenameCommand , function(log){
+          resolve(log);
+        });
+      });
+
     }).then(function(log){
         console.log("Building Application.....");
         // Now build the application
         return new Promise(function(resolve,reject){
           console.log(buildCommand);
           cmd.get(buildCommand,function(log){
+            console.log(log);
             console.log("Application Built!!");
             resolve();
           });
