0cbb4f542e666dba6bfbac5eba8ab1494f6bcd9b
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2017-01-09 05:50:42 +0530

    Add comments

--- shell_scripts/copy_replace_script.sh
+++ shell_scripts/copy_replace_script.sh
@@ -4,13 +4,16 @@ if test $# -ge 2; then
 	sourceFile=$1
 	destinationFile=$2
 
+	# ========= REMOVE DESTINATION FILE ===========
 	echo "Removing file.."
 	rm "$destinationFile"
 	echo "File deleted"
 
+	# ========= COPY THE SOURCE FILE =============
 	echo "copying files"
 	cp "$sourceFile" "$destinationFile"
 	echo "copied file"
+	
 else
 	echo "Invalid number of arguments"
 fi
