78566d8b8acd35bfbe288a518fd2f658b19b212f
utkarshnath <nath.utkarsh1@gmail.com>
2017-01-14 17:17:31 +0530

    Added post api

---
+++ api/v1/post.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var post = require('../../models/post');
+
+var router = express.router();
+
+router.post('/',function(req,res){
+
+	var timeStamp = req.body.timeStamp;
+	var appId = req.body.appId;
+
+	var query = post.find({'appId' : appId}).
+					where('timeStamp').gt(timeStamp).
+					sort('timeStamp');
+	query.select(*);
+	query.exec(function(err,post[]){
+		// if(err)return handleError(err);
+		jsonObj = [];
+		for(let i=0;i<post.length;i++){
+			postItem = {};
+			postItem['appId'] = post[i]['appId'];
+			postItem['userId'] = post[i]['userId'];
+			postItem['mimeType'] = post[i]['mimeType'];
+			postItem['timeStamp'] = post[i]['timeStamp'];
+			postItem['locationUri'] = post[i]['locationUri'];
+			postItem['description'] = post[i]['description'];
+			postItem['likes'] = post[i]['likes'];
+			jsonObj.push(postItem);
+		}
+		return res.json(jsonObj);
+	})
+
+})
